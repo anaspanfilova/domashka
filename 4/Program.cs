47074329e8ffc91776faa811c700fa161f3f85ed
Анастасия <understooooood@mail.ru>
2022-10-07 10:07:22 +0300

    решили четвертую задачу

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает трехзначное число, а на выходе показывает последнюю цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{x%10}");
