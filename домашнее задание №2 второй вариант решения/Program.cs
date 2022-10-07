030de5b7f913ae8e49a30731145a6a5abae035a4
Анастасия <understooooood@mail.ru>
2022-10-07 14:51:15 +0300

    решили вторую задачу из домашнего задания другим способом

---
+++ домашнее задание №2 второй вариант решения/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int y = int.Parse(Console.ReadLine()!);
+if (x > y)
+{
+Console.WriteLine($"{x} наибольшее, а {y} наименьшее");
+}
+else
+{
+   Console.WriteLine($"{y} наибольшее, а {x} наименьшее"); 
+}
