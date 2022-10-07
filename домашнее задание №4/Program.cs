d271540309616b9fb1a61352a952e630a0ff0919
Анастасия <understooooood@mail.ru>
2022-10-07 14:50:02 +0300

    решили четвертую задачу из домашнего задания

---
+++ домашнее задание №4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает три числа и выдает максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int z = int.Parse(Console.ReadLine()!);
+int max = x;
+if (y > x)
+    max = y;
+else 
+{
+    max = x;
+}
+    if (z > max)
+        max = z;
+Console.WriteLine($"{max}");
