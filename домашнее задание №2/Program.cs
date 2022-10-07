e79b656ef9b0f3bd30e719381eb6520c9d6b4b45
Анастасия <understooooood@mail.ru>
2022-10-07 13:12:20 +0300

    решили вторую задачу из домашнего задания

---
+++ домашнее задание №2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше.
+
+Console.WriteLine("Введите первое число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int y = int.Parse(Console.ReadLine()!);
+int max = x;
+if (x > max)
+{
+Console.WriteLine($"{x} = max");
+}
+else
+{
+   Console.WriteLine($"{y} = max"); 
+}
