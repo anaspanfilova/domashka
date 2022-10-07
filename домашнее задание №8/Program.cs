e47ca6005de76f68b683ad9061308362d8ce1179
Анастасия <understooooood@mail.ru>
2022-10-07 15:23:44 +0300

    решили восьмую задачу из домашнего задания

---
+++ домашнее задание №8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int X = int.Parse(Console.ReadLine()!);
+int N2 = 2;
+while(N2 <= X)
+{
+    Console.Write($"{N2} ");
+    N2 = N2 + 2;
+}
